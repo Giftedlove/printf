35d37a61dff1f0fd2bbe01a4e5896ff3db74ae90
Solomon-dev98 <asapnino01@gmail.com>
2023-09-19 13:37:05 -0700

    corrections

--- _printchar.c
+++ _printchar.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - the main function
+ * print_char - the main function
+ * @ap: a variable list
  * Return: 1
  */
 
 int print_char(va_list ap)
 {
-    int ch = va_arg(ap, int);
-    _putchar(ch);
-    return (1);
+	int ch = va_arg(ap, int);
+
+	_putchar(ch);
+	return (1);
 }
--- _putchar.c
+++ _putchar.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <unistd.h>
 
+/**
+ * _putchar - the function that prints a single char.
+ * @c: a variable of type char.
+ * Return: write
+ */
+
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 
 }
