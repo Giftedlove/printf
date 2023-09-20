c3f29766eb1fece7251c75408f97e5d34c78f946
Giftedlove <lovedaniel0813@gmail.com>
2023-09-19 19:58:27 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -5,44 +5,50 @@
  * @format: the method it should be
  * Return: lenght
  */
+int _printf(const char *format, ...);
 int _printf(const char *format, ...)
 {
 	va_list ap;
 	int length = 0;
 
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
 	va_start(ap, format);
 
 	while (*format)
 	{
-	if (*format == '%' && *(format + 1) != '\0')
-	{
-		++format;
-		switch (*format++)
+		if (*format == '%' && *(format + 1) != '\0')
 		{
-			case '%':
-			length += _putchar('%');
-			break;
-			case 'c':
-			length += print_char(ap);
-			break;
-			case 'i':
-			case 'd':
-			length += print_decimal(ap);
-			break;
-			case 's':
-			length += print_string(ap);
-			break;
-			default:
-			length += _putchar('%');
-			_putchar(*(format - 1));
-			++length;
-			break;
+			++format;
+			switch (*format++)
+			{
+				case '%':
+					length += _putchar('%');
+					break;
+				case 'c':
+					length += print_char(ap);
+					break;
+				case 'i':
+				case 'd':
+					length += print_decimal(ap);
+					break;
+				case 's':
+					length += print_string(ap);
+					break;
+				default:
+					length += _putchar('%');
+					_putchar(*(format - 1));
+					++length;
+					break;
+			}
 		}
-	}
-	else
-	{
-		length += _putchar(*format++);
-	}
+		else
+		{
+			length += _putchar(*format++);
+		}
 	}
 	va_end(ap);
 	return (length);
